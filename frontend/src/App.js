49d06bd6e80b15e3a47a0664367cdb9fa5ba721b
Leander Steiner <leandersteiner@yahoo.de>
2021-05-22 13:53:31 +0200

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { CreatePost } from './CreatePost';
+import { PostList } from './PostList';
+
+export const App = () => {
+    return (
+        <div className="container">
+            <h1>Create Post</h1>
+            <CreatePost />
+            <hr />
+            <h1>Posts</h1>
+            <PostList />
+        </div>
+    );
+}
---
+++ post/index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import cors from 'cors';
+import { randomBytes } from 'crypto';
+import axios from 'axios';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/posts', async (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+
+    posts[id] = {
+        id,
+        title
+    };
+
+    await axios.post("http://localhost:4005/events", {
+        type: "PostCreated",
+        data: {
+          id,
+          title,
+        },
+    });
+
+    res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+    console.log("Received Event", req.body.type);
+  
+    res.send({});
+  });
+
+app.listen(4000, () => {
+    console.log("Post Service listening on port 4000");
+});
